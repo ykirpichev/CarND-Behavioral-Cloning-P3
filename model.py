16f3a6aa4c6ba558ecc50b197dfd561f132843df
Yury Kirpichev <ykirpichev@yandex-team.ru>
2017-07-18 01:44:23 +0300

    Lates changes

--- model.py
+++ model.py
@@ -1,27 +1,46 @@
 import csv
 import cv2
 import os
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
+import click
 
-samples = []
+
 batch_size = 64
 
-corrections = [0, .2, -0.2]
-with open('data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        for index in range(3):
-            samples.append([line[index], float(line[3]) + corrections[index]])
+def load_and_augment_data():
+    samples = []
+    corrections = [0, .2, -0.2]
+    with open('data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            for index in range(3):
+                samples.append([line[index], float(line[3]) + corrections[index]])
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    return samples
 
+samples = load_and_augment_data()
 make_path = lambda x: os.path.join('data/IMG', x.split('/')[-1])
 read_image = lambda x: cv2.cvtColor(cv2.imread(make_path(x)), cv2.COLOR_BGR2RGB)
 
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def show_stats():
+    import matplotlib.pyplot as plt
+    angles = []
+    for sample in samples:
+        angles.append(sample[1])
+    plt.hist(angles, normed=True, bins=25)
+    plt.ylabel('Count');
+    plt.xlabel('Angle');
+    plt.show()
 
 def generator(samples, batch_size):
+    from sklearn.utils import shuffle
+    import numpy as np
+
+
     num_samples = len(samples)
     while 1:
         shuffle(samples)
@@ -44,41 +63,55 @@ def generator(samples, batch_size):
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=batch_size)
-validation_generator = generator(validation_samples, batch_size=batch_size)
+def make_model():
+    from keras.models import Sequential
+    from keras.layers import Flatten, Dense, Lambda, Dropout
+    from keras.layers import Conv2D, Cropping2D, MaxPooling2D
+    
+    
+    model = Sequential()
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+    model.add(Conv2D(3, (5, 5), strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(24, (5, 5), strides=2, padding='same', activation='relu'))
+    model.add(Conv2D(36, (5, 5), strides=2, padding='same', activation='relu'))
+    model.add(Conv2D(48, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+@cli.command()
+def train_model():
+    from sklearn.model_selection import train_test_split
+
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
+
+    model = make_model()
+
+    history_object = model.fit_generator(train_generator,
+            steps_per_epoch = len(train_samples) / batch_size,
+            validation_data = validation_generator,
+            validation_steps = len(validation_samples) / batch_size,
+            epochs = 5,
+            verbose = 1)
+
+    model.save('model.h5')
 
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout
-from keras.layers import Conv2D, Cropping2D, MaxPooling2D
-
-
-model = Sequential()
-model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Conv2D(3, (5, 5), strides=1, padding='same', activation='relu'))
-model.add(Conv2D(24, (5, 5), strides=2, padding='same', activation='relu'))
-model.add(Conv2D(36, (5, 5), strides=2, padding='same', activation='relu'))
-model.add(Conv2D(48, (3, 3), padding='same', activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-
-
-model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator,
-        steps_per_epoch = len(train_samples) / batch_size,
-        validation_data = validation_generator,
-        validation_steps = len(validation_samples) / batch_size,
-        epochs = 5,
-        verbose = 1)
-
-model.save('model.h5')
+@cli.command()
+def draw_model():
+    model = make_model()
 
 #print(history_object.history.keys())
 #
@@ -91,3 +124,6 @@ model.save('model.h5')
 #plt.xlabel('epoch')
 #plt.legend(['training set', 'validation set'], loc='upper right')
 #plt.show()
+
+if __name__ == '__main__':
+    cli()
